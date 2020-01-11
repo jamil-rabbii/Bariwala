11b6efcaef4e3dfe07efd3b751f2cb3c1393053c
Saima98 <saimanajmin750@gmail.com>
2020-01-12 03:29:08 +0600

    home frontView done

---
+++ resources/views/frontView/inc/footer.blade.php
@@ -0,0 +1,5 @@
+
+		<footer class="footer-down">
+			<p>&copy 2017 <span>ReaLand</span> Real Estate. All Right Reserved.</p>
+		</footer>
+		
---
+++ resources/views/frontView/inc/header.blade.php
@@ -0,0 +1,23 @@
+<!--HEADER SECTION START-->
+		<header class="header-area">
+			<div class="header-top">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 col-sm-12">
+							<a href="#"><i></i>(+88) 01712345678</a>
+							<a href="#"><i></i>realland@gmail.com</a>X
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-12">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="header-down">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 col-sm-12">
+							<div class="logo">
+								<a href="#home"><span>land</span>load</a>
+							</div>
+						</div>
