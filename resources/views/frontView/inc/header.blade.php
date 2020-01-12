856ee71f4ec2c7be4fb2b611230a35d10aaa5608
Saima98 <saimanajmin750@gmail.com>
2020-01-13 02:27:04 +0600

    updated

--- resources/views/frontView/inc/header.blade.php
+++ resources/views/frontView/inc/header.blade.php
@@ -5,7 +5,7 @@
 					<div class="row">
 						<div class="col-lg-6 col-md-6 col-sm-12">
 							<a href="#"><i></i>(+88) 01712345678</a>
-							<a href="#"><i></i>realland@gmail.com</a>X
+							<a href="#"><i></i>realland@gmail.com</a>
 						</div>
 						<div class="col-lg-6 col-md-6 col-sm-12">
 							
